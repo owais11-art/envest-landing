b27edf840c43b75f93112c1ee837e9ffe807f1c0
owais_zahoor <owaiszahoor79@gmail.com>
2022-07-08 10:43:51 +0530

    jgsdu

--- _site/assets/js/script.js
+++ _site/assets/js/script.js
@@ -1,13 +1,21 @@
-const toggler = document.querySelector('.nav__container__toggler');
+const toggler = document.querySelector(".nav__container__toggler");
 
-const cancel = document.querySelector('.side-nav__container__cancel');
+const cancel = document.querySelector(".side-nav__container__cancel");
 
-const sideBar = document.querySelector('.side-nav');
+const sideBar = document.querySelector(".side-nav");
 
-toggler.addEventListener('click', () => {
-    sideBar.classList.add('active');
+const button = document.querySelectorAll("button");
+console.log("button");
+
+toggler.addEventListener("click", () => {
+  sideBar.classList.add("active");
 });
 
-cancel.addEventListener('click', () => {
-    sideBar.classList.remove('active');
+cancel.addEventListener("click", () => {
+  sideBar.classList.remove("active");
+});
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("clicked");
 });
