eda0bfdafc06404a6bc51fcddff6058d10b068e7
owais_zahoor <owaiszahoor79@gmail.com>
2022-07-13 17:50:52 +0530

    added fields to form

--- _site/assets/js/mail.js
+++ _site/assets/js/mail.js
@@ -1,19 +1,28 @@
-const input = document.querySelector("input");
+const input = document.querySelector("#mail");
+const fName = document.querySelector("#first-name");
+const lName = document.querySelector("#last-name");
+const phone = document.querySelector("#mobile");
+const message = document.querySelector("#msg");
 const form = document.querySelector("form");
 
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  let tempParams = {
-    from_name: "owais khan",
-    message: `${input.value} subscribed`
-  };
-  emailjs.send("service_cxs2chw","template_rou68ij", tempParams)
-  .then(res => {
-    if(res.status === 200) {
-      input.value = '';
-      window.location.href += '/thanks.html';
-    }
-    else console.log('Email not sent😞');
+if(form){
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let tempParams = {
+      from_name: "owais khan",
+      message: `${input.value} subscribed`,
+    };
+    emailjs
+      .send("service_cxs2chw", "template_rou68ij", tempParams)
+      .then((res) => {
+        if (res.status === 200) {
+          input.value = "";
+          fName.value = "";
+          lName.value = "";
+          phone.value = "";
+          message.value = "";
+          window.location.href += "/thanks.html";
+        } else console.log("Email not sent😞");
+      });
   });
-});
+}
