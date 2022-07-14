bb29a6add7ea4e8a1d8919ef0337403f70862d54
owais_zahoor <owaiszahoor79@gmail.com>
2022-07-14 16:43:38 +0530

    validated

--- _site/assets/js/mail.js
+++ _site/assets/js/mail.js
@@ -5,53 +5,115 @@ const phone = document.querySelector("#mobile");
 const message = document.querySelector("#msg");
 const form = document.querySelector("form");
 
+input.addEventListener("change", (e) => {
+  formValidation(e.target);
+});
+
+phone.addEventListener("change", (e) => {
+  formValidation(e.target);
+});
+
 const setError = (element, errorMessage) => {
-  element.parentElement.style.borderBottom = '1px solid orangered';
-  if(!element.parentElement.nextElementSibling.firstElementChild.firstElementChild){
-    const i = document.createElement('i');
-    i.classList.add('bi' ,'bi-exclamation-octagon-fill');
-    i.style.color = 'orangered';
+  element.parentElement.style.borderBottom = "1px solid orangered";
+  if (
+    !element.parentElement.nextElementSibling.firstElementChild
+      .firstElementChild
+  ) {
+    const i = document.createElement("i");
+    i.classList.add("bi", "bi-exclamation-octagon-fill");
+    i.style.color = "orangered";
     console.log(i);
     element.parentElement.nextElementSibling.firstElementChild.append(i);
-    element.parentElement.nextElementSibling.lastElementChild.textContent = errorMessage;
-  }else element.parentElement.nextElementSibling.lastElementChild.textContent = errorMessage;
-  element.classList.add('error');
-}
+    element.parentElement.nextElementSibling.lastElementChild.textContent =
+      errorMessage;
+  } else
+    element.parentElement.nextElementSibling.lastElementChild.textContent =
+      errorMessage;
+  element.classList.add("error");
+};
 
-const formValidation = () => {
-  const emailValidator = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-  if(input.value){
-    if (!emailValidator.test(input.value)) {
-      setError(input, 'Invalid Email');
+const resetError = (element) => {
+  element.parentElement.style.borderBottom = "1px solid #959EAC";
+  if (
+    element.parentElement.nextElementSibling.firstElementChild.firstElementChild
+  ) {
+    element.parentElement.nextElementSibling.firstElementChild.remove(
+      element.parentElement.nextElementSibling.firstElementChild
+        .firstElementChild
+    );
+  }
+  element.parentElement.nextElementSibling.firstElementChild.textContent &&
+    (element.parentElement.nextElementSibling.firstElementChild.textContent =
+      "");
+};
+
+const emailValidation = (value, element) => {
+  const emailValidator = new RegExp(
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  );
+  if (value) {
+    if (!emailValidator.test(value)) {
+      setError(element, "Invalid Email");
       return false;
-    };
-  }else {
-    setError(input, 'Cannot be Empty');
+    } else {
+      resetError(element);
+      return true;
+    }
+  } else {
+    setError(element, "Cannot be Empty");
     return false;
   }
+};
 
-  if(phone.value){
-    const isNum = phone.value.split('').some(val => !Number.isInteger(parseInt(val)));
-    if(isNum) {
-      setError(phone, 'input must be digits');
-      return false;
-    }
-    if(phone.value.length !== 10){
-      setError(phone, 'should be 10 digits');
-      return false;
-    }
-  }else {
-    setError(phone, 'Cannot be Empty');
+const phoneValidation = (phone) => {
+  const length = phone.value.length;
+  let slicedValue;
+  if(length === 13){
+    slicedValue = phone.value.slice(3);
+  }else if(length === 12){
+    slicedValue = phone.value.slice(2);
+  }else{
+    setError(phone, "should be 10 digits");
     return false;
   }
-  return true;
+  if (slicedValue) {
+    const isNum = slicedValue
+      .split("")
+      .some((val) => !Number.isInteger(parseInt(val)));
+    if (isNum) {
+      setError(phone, "input must be digits");
+      return false;
+    } else {
+      resetError(phone);
+      return true;
+    }
+  } else {
+    setError(phone, "Cannot be Empty");
+    return false;
+  }
+};
+
+const formValidation = (target = null) => {
+  let emailValidate, phoneValidate;
+  if (!target) {
+    emailValidate = emailValidation(input.value, input);
+    phoneValidate = phoneValidation(phone);
+  } else if (target && target === input) {
+    emailValidate = emailValidation(input.value, input);
+  } else if (target && target === phone) {
+    phoneValidate = phoneValidation(phone);
+  }
+
+  if (emailValidate && phoneValidate) return true;
+
+  return false;
 };
 
 if (form) {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const validate = formValidation();
-    if(validate){
+    if (validate) {
       let tempParams = {
         from_name: "owais khan",
         message: `${fName.value} ${lName.value} subscribed`,
